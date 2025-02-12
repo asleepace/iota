42ccf6bb45f33cad0db9d07e2c350f4884051fae
Colin <colin_teahan@yahoo.com>
2025-02-11 21:59:38 -0800

    basic project initialized

--- Package.swift
+++ Package.swift
@@ -1,14 +1,41 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "iota",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+        
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "iota"),
-    ]
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ],
+    swiftLanguageModes: [.v5]
 )
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
---
+++ README.md
@@ -0,0 +1,22 @@
+# iota
+
+Start a webserver on http://127.0.0.1:8080
+
+```bash
+# build and run
+swift build
+swift run
+
+# clean, update and build
+swift package clean
+swift package update
+swift package build
+
+# create a new package
+swift package init --type executable
+```
+
+## References
+
+- https://docs.vapor.codes/getting-started/hello-world/
+- https://github.com/vapor/template-bare
